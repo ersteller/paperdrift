b59931df623b1022e51d94d483a1472d744d400f
ersteller <siggi2017@freenet.de>
2020-10-17 03:32:15 +0200

    fixed rotation and constraints

--- Assets/InputScrip.cs
+++ Assets/InputScrip.cs
@@ -4,55 +4,62 @@ using UnityEngine;
 
 public class InputScrip : MonoBehaviour
 {
-    public Transform target;
+    public Transform car;
+    public Transform puller;
     public float timespeed;
 
     private Camera cam;
 
     private float friction;
 
-    // private double inertia = 0.001; // kg
-    // private double inertiaRot = 0.001; // kg*m^2
+    private Vector3 cartopullerrel;
+
+
 
     private Vector3 velocity;
     private Vector3 velocityLast;
-    private float rotationspeed; // positive counterclockwise
-    private float rotationspeedLast; // positive counterclockwise
 
-    private Vector3 mousePos;
+    // those are in worldspace units 
+    private Vector3 mousePos; 
     private Vector3 mousePosLast;
     private Vector3 mouseVel;
     private Vector3 mouseVelLast;
     private Vector3 mouseAccel;
+    private Vector3 carPos;
+    private Vector3 carPosLast;
+    private Vector3 carVel;
+    private Vector3 carVelLast;
+    private Vector3 carAccel;
+    private Vector3 accelComponents;
 
-    private float offset = 2; // 2 units offset
+    private float caroffset = 1.5f; // 2 Worldunits offset from the car to the puller 
 
-    private Vector3 force;
-    private float mass = 0.1F;   // mass of target
-    private float inertia = 0.1F; // rotaional mass
-    private float angularspeed = 0;
+    private float mass = 1;       // mass of car 
+    private Vector3 force;        // applied to car
     private float angle = 0;
-    private float angularacceleration = 0;
 
-
-    private Vector3 vectrotate;
     Quaternion quatrotate;
 
     void Start()
     {
         cam = Camera.main;
-        vectrotate.x = 0;
-        vectrotate.y = 0;
-        vectrotate.z = 0;
         quatrotate = Quaternion.Euler(0, 0, 0);
         timespeed = 1F;
         friction = 100;
+        carVel = new Vector3(0, 0, 0);
+        carPos = car.position;
+
+        // car.position -2
+        // initialize mouseposition so we dont get a spike in fisrt update
+        float xpos = Input.mousePosition.x;
+        float ypos = Input.mousePosition.y;
+        mousePos = cam.ScreenToWorldPoint(new Vector3(xpos, ypos, cam.nearClipPlane));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         float step = timespeed * Time.deltaTime;
 
         float xpos = Input.mousePosition.x;
@@ -68,42 +75,73 @@ public class InputScrip : MonoBehaviour
         // calk accelleration of input
         mouseAccel = (mouseVel - mouseVelLast) / step;
 
-        // speed, acceleration, force friction and torque are guing into physical model
-        // we need to split acceleration components in axis of target (CoG)
-        // we do that by rotating the axis of the applied force by the amount of the target 
-        // rotation so the components are aligned and othogonal with the target
+        // speed, acceleration, force friction are guing into physical model
+        // we need to split acceleration components in axis of car (CoG)
+        // we do that by rotating the axis of the applied force by the amount of the car.rotation
+        // so the components are aligned and othogonal with the car
+
+        // angel of car = -atan2(Rx,Ry)  // found to be empirically correct (negative because 3d z-axis)
+        cartopullerrel = puller.position - car.position;
+        angle = -Mathf.Atan2(cartopullerrel.x, cartopullerrel.y);
+
+        // Get acceleration component in car koordinates
+        accelComponents = Vector3Extension.Rotate(mouseAccel, -angle);
+
+        // calk car force F=m*a (y because of car initial orientation pointing up)
+        float carforce = mass * accelComponents.y;
+        carAccel = Vector3Extension.Rotate(new Vector3(0, carforce, 0), angle); // rotate acceleration back to world coordinates
+
+        // integrate force to get velocity
+        carVel.x = (carAccel.x / mass) * step + carVel.x;
+        carVel.y = (carAccel.y / mass) * step + carVel.y;
+
+        // constrain to mouse velocity in y axis(car)
+        Vector3 carvelComponents = Vector3Extension.Rotate(carVel, -angle);
+        Vector3 mousvelComponents = Vector3Extension.Rotate(mouseVel, -angle);
+        carvelComponents.y = mousvelComponents.y;
         
-        // calk force F=m*a
-        force.x = mouseAccel.x * mass;
-        force.y = mouseAccel.y * mass;
-
-        // calk torque = (F)x(r)
-        angle = target.eulerAngles.z * Mathf.Deg2Rad;
-        Vector3 forceComponents = Vector3Extension.Rotate(force, angle);
-        float torque = forceComponents.x * offset; // only this component is adding spin to the target.
-        // calc angular acceleration     Torque = inertia * omega' 
-        angularacceleration = torque / inertia;
-
-        // dont forget dt when integrating (this is no simulation but anyways)
-        angularspeed += angularacceleration * step;
-        angle += angularspeed * step;
-
-        // also reduce angular speed by friction linearly
-        if (angularspeed > 0)
+        // reduce velocity by friction linearly in x axis(car)
+        if (carvelComponents.x > friction * step)
         {
-            angularspeed -= friction * step;
-        } else if (angularspeed < 0)
-        {
-            angularspeed += friction * step;
+            carvelComponents.x -= friction * step;
         }
-         
-        //update the position
+        else if (carvelComponents.x < -friction * step)
+        {
+            carvelComponents.x += friction * step;
+        }
+        else if ((carvelComponents.x < friction * step && carvelComponents.x > 0) || (carvelComponents.x > -friction * step && carvelComponents.x < 0))
+        {
+            carvelComponents.x = 0;
+        }
 
+        // friction is overwritten by mouse in y
+
+        // convert new velocity to world koordinates
+        carVel = Vector3Extension.Rotate(carvelComponents, angle);
+   
+        // integrate velocity to get position
+        carPos.x = carVel.x * step + carPos.x;
+        carPos.y = carVel.y * step + carPos.y;
+
+        // set car to distance offset from puller 
+        Vector3 pullertocar = carPos - puller.position;
+        pullertocar.Normalize();
+        pullertocar = pullertocar * caroffset; // TODO: this is still not working as expected
+        // set car position to 
+        carPos = pullertocar + puller.position;
+        
+        //update the position of puller (under the mouse)
+        puller.position = cam.ScreenToWorldPoint(new Vector3(xpos, ypos, cam.nearClipPlane));
+
+        // update rotation (always pointing to puller)
         Quaternion tempquatrotate = Quaternion.Euler(0,0, angle* Mathf.Rad2Deg);
-        target.rotation = tempquatrotate;
-        // transform.position = cam.ScreenToWorldPoint(new Vector3(xpos, ypos, cam.nearClipPlane));
-        target.position = cam.ScreenToWorldPoint(new Vector3(xpos, ypos, cam.nearClipPlane));
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        car.rotation = tempquatrotate;
+
+        // update position of car
+        float maxdistance = 2;
+        
+        car.position = Vector3.MoveTowards(car.position, carPos, maxdistance);
+
     }
 
     void OnGUI()
@@ -120,15 +158,14 @@ public class InputScrip : MonoBehaviour
 
         point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
 
-        GUILayout.BeginArea(new Rect(20, 20, 350, 220));
-        GUILayout.Label("Screen pixels:         " + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.BeginArea(new Rect(20, 20, 450, 220));
         GUILayout.Label("Mouse Pixel Position:  " + mousePos);
         GUILayout.Label("Mouse World Position:  " + point.ToString("F3"));
         GUILayout.Label("Mouse World Speed:     " + mouseVel.ToString("F3"));
         GUILayout.Label("Mouse World Accel.:    " + mouseAccel.ToString("F3"));
-        GUILayout.Label("Target World Rot.:     " + target.rotation.ToString("F3") + angle);
-        GUILayout.Label("Target World RotSpeed: " + angularspeed);
-        GUILayout.Label("Target WorldRotAccel.: " + angularacceleration);
+        GUILayout.Label("carkoord forces:       " + accelComponents.ToString("F3"));
+        GUILayout.Label("carAccel.:             " + carAccel.ToString("F3"));
+        GUILayout.Label("carspeed :             " + carVel.ToString("F3"));
         GUILayout.EndArea();
     }
 }
---
+++ Assets/debugsprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugsprite : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform debug;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion tempquatrotate = Quaternion.Euler(0, 0, target.rotation.eulerAngles.z); // euler gets degrees and setts degrees
+        debug.rotation = tempquatrotate;
+    }
+}
